ab4051ddcbcee35d325abac5f82d252e2cf3e93f
juhyeok5185 <117324639+juhyeok5185@users.noreply.github.com>
2022-12-08 14:51:12 +0900

    Added

---
+++ Java/data/ex01/src/ex01/Ex05.java
@@ -0,0 +1,17 @@
+package ex01;
+
+public class Ex05 {
+
+	public static void main(String[] args) {
+		int a = 10;
+		int b = 20;
+		int c = 30;
+		int d = 40;
+		int e = 50;
+		System.out.println(a<b && a>c);
+		System.out.println(a<b || c<d);
+		System.out.println(!(c<d));
+		
+	}
+
+}
---
+++ Java/data/ex01/src/ex01/Ex06.java
@@ -0,0 +1,18 @@
+package ex01;
+
+public class Ex06 {
+
+	public static void main(String[] args) {
+		String name = "홍길동";
+		int age = 27;
+		float weight = 77.4f;
+		boolean marriage = true;
+		
+		System.out.println("이름 : "+ name + "입니다");
+		System.out.println("나이 : " + age);
+		System.out.println("몸무게 :" + weight);
+		System.out.println("결혼 :" + marriage);
+		
+	}
+
+}
